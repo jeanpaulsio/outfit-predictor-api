3fb8d04f73ff4a39863851aeafa4a9c0adb97ba4
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-02-24 12:24:15 -0800

    updates seed file with pants and shirt colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
-shirt_colors = %w[gray pink black white black denim blue red]
+Shirt.destroy_all
+Pant.destroy_all
+
+shirt_colors = %w[gray pink black white denim blue red]
 pant_colors  = %w[lightgreen gray olive denim brown]
+
+shirt_colors.each do |color|
+  Shirt.create!(color: color)
+end
+
+pant_colors.each do |color|
+  Pant.create!(color: color)
+end

55a0a3dabdc45bb75707e599ba4531cf1ece4b2e
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-02-25 01:38:04 -0800

    testing home route with react-router

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
           <div>
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <Link to="/home">Home</Link>
               </li>
               <li>
                 <Link to="/about">About</Link>

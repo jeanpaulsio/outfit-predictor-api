e9f2fcc54d820d3da8f8d915c08f5b4a77b07676
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-02-25 18:20:10 -0800

    adds route for color picker

--- client/src/containers/Router.js
+++ client/src/containers/Router.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Main from "./Main";
+import ColorPicker from "./ColorPicker";
 
 function Router() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Main} />
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/color-picker">Color Picker</Link></li>
+        </ul>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/color-picker" component={ColorPicker} />
+      </div>
     </BrowserRouter>
   );
 }

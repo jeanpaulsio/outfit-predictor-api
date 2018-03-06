e223893f9c42aac54b42cbdffd6d08e104be83bf
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-03-05 22:06:32 -0800

    styling for home page - dynamic title and favicons displayed

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { object, func } from "prop-types";
 import { connect } from "react-redux";
-import moment from "moment";
-import { Container } from "semantic-ui-react";
+import "./Main.css";
 
 import * as actions from "../actions";
 import Shirt from "../components/Shirt";
 import Pants from "../components/Pants";
+
+import Favicon from "react-favicon"
 
 class Main extends Component {
   static propTypes = {
@@ -20,12 +21,11 @@ class Main extends Component {
 
   render() {
     return (
-      <Container>
-        <h1>Outfit Predictor</h1>
-        <h2>Today is a {moment().format('dddd')}</h2>
+      <section className="main-container">
+        <Favicon url={`favicons/${this.props.prediction.shirt}.png`} />
         <Shirt fill={this.props.prediction.shirt} />
         <Pants fill={this.props.prediction.pants} />
-      </Container>
+      </section>
     );
   }
 }
--- client/src/containers/Router.js
+++ client/src/containers/Router.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import DocumentTitle from "react-document-title";
+import moment from "moment";
+
 import Main from "./Main";
 import ColorsIndex from "./ColorsIndex";
 
 function Router() {
   return (
-    <BrowserRouter>
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/colors">Colors</Link></li>
-        </ul>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/colors" component={ColorsIndex} />
-      </div>
-    </BrowserRouter>
+    <DocumentTitle title={moment().format("dddd MMM Do")}>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/colors" component={ColorsIndex} />
+        </div>
+      </BrowserRouter>
+    </DocumentTitle>
   );
 }
 
